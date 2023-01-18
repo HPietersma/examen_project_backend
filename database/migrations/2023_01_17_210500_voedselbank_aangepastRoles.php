e13e37be75615fda5a8ab2f1ff078b553acc1a17
HPietersma <hessel.pietersma@hotmail.nl>
2023-01-18 13:25:19 +0100

    missing migration and seeder

--- database/migrations/2023_01_17_210500_voedselbank_aangepastRoles.php
+++ database/migrations/2023_01_17_210500_voedselbank_aangepastRoles.php
@@ -19,7 +19,7 @@ return new class extends Migration
          */
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('rolnaam');
+            $table->string('role_name');
         });
 
         /**
